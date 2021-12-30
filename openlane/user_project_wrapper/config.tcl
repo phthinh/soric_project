5ce123ee9a8e85e4ee58afb919328565bcb5a1ae
phthinh <hung3@e.ntu.edu.sg>
2021-12-30 00:02:20 +0000

    add config for hardening macros

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -29,19 +29,23 @@ set script_dir [file dirname [file normalize [info script]]]
 
 set ::env(DESIGN_NAME) user_project_wrapper
 #section end
-
+set ::env(ROUTING_CORES)   8
 # User Configurations
 
 ## Source Verilog Files
 set ::env(VERILOG_FILES) "\
 	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/user_project_wrapper.v"
+	$script_dir/../../verilog/rtl/user_project_wrapper.v \
+	$script_dir/../../verilog/rtl/soric_core.v"
 
 ## Clock configurations
-set ::env(CLOCK_PORT) "user_clock2"
-set ::env(CLOCK_NET) "mprj.clk"
+set ::env(CLOCK_PORT)   "wb_clk_i"
+set ::env(CLOCK_NET)    "wb_clk_i"
+set ::env(CLOCK_PERIOD) "100"
 
-set ::env(CLOCK_PERIOD) "10"
+# The following is because there are no std cells in the example wrapper project.
+#set ::env(SYNTH_TOP_LEVEL) 1
+#set ::env(SYNTH_NO_FLAT) 1
 
 ## Internal Macros
 ### Macro PDN Connections
@@ -51,35 +55,50 @@ set ::env(FP_PDN_MACRO_HOOKS) "\
 ### Macro Placement
 set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
 
-### Black-box verilog and views
-set ::env(VERILOG_FILES_BLACKBOX) "\
-	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/user_proj_example.v"
-
-set ::env(EXTRA_LEFS) "\
-	$script_dir/../../lef/user_proj_example.lef"
-
-set ::env(EXTRA_GDS_FILES) "\
-	$script_dir/../../gds/user_proj_example.gds"
-
 set ::env(GLB_RT_MAXLAYER) 5
 
 # disable pdn check nodes becuase it hangs with multiple power domains.
 # any issue with pdn connections will be flagged with LVS so it is not a critical check.
 set ::env(FP_PDN_CHECK_NODES) 0
 
-# The following is because there are no std cells in the example wrapper project.
-set ::env(SYNTH_TOP_LEVEL) 1
-set ::env(PL_RANDOM_GLB_PLACEMENT) 1
 
-set ::env(PL_RESIZER_DESIGN_OPTIMIZATIONS) 0
+#set ::env(PL_RANDOM_GLB_PLACEMENT) 1
+
+
+## Placement
+set ::env(PL_TARGET_DENSITY)     0.15
+# Increase diamanod search height so that the detailed placement can legalize cells overlapping with big macros
+#set ::env(PL_DIAMOND_SEARCH_HEIGHT) 400	
+set ::env(CELL_PAD)              0
+
+#set ::env(PL_RESIZER_DESIGN_OPTIMIZATIONS) 0
 set ::env(PL_RESIZER_TIMING_OPTIMIZATIONS) 0
-set ::env(PL_RESIZER_BUFFER_INPUT_PORTS) 0
-set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
+#set ::env(PL_RESIZER_BUFFER_INPUT_PORTS) 0
+#set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
 
 set ::env(FP_PDN_ENABLE_RAILS) 0
 
-set ::env(DIODE_INSERTION_STRATEGY) 0
+set ::env(DIODE_INSERTION_STRATEGY) 4
 set ::env(FILL_INSERTION) 0
 set ::env(TAP_DECAP_INSERTION) 0
 set ::env(CLOCK_TREE_SYNTH) 0
+
+### Black-box verilog and views
+set ::env(VERILOG_FILES_BLACKBOX) "\
+	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
+	$script_dir/../../verilog/rtl/soric/soric_soc.v \
+	$script_dir/../../verilog/rtl/cores/ibex_top.v \
+	$script_dir/../../verilog/rtl/cores/flexbex_ibex_core.v \
+	$script_dir/../../verilog/rtl/mem/sky130_sram_2kbyte_1rw1r_32x512_8.v"
+
+set ::env(EXTRA_LEFS) "\
+	$script_dir/../../lef/soric_soc.lef \
+	$script_dir/../../lef/ibex_top.lef \
+	$script_dir/../../lef/flexbex_ibex_core.lef \
+	$script_dir/../../lef/sky130_sram_2kbyte_1rw1r_32x512_8.lef"
+
+set ::env(EXTRA_GDS_FILES) "\
+        $script_dir/../../gds/soric_soc.gds \
+        $script_dir/../../gds/ibex_top.gds \
+	$script_dir/../../gds/flexbex_ibex_core.gds \
+	$script_dir/../../gds/sky130_sram_2kbyte_1rw1r_32x512_8.gds"
